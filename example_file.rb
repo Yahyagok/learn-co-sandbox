8bd32faa5e7f5f6f1e91c9972284382601a4b86c
Yahya  Gok <ygok17@hotmail.com>
2021-06-18 04:23:53 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,23 @@
+
+
+def hi
+fruits =  ["apple", "pear", "face", "champagne", "palm tree", "aardvark", "pineapple"]
+
+fruits.select do |fruit|
+  fruit.start_with?("a")
+end 
+end 
+
+def word_count(sentence)
+  sentence.split(" ").count 
+end
+
+def hi 
+foods = {"pie" => "delicious", "broccoli" => "not delicious",
+"carrots" => "not delicious", "apples" => "delicious",
+"peanut butter" => "delicious"}
+
+foods.each do  | key, value |  
+  if value == delicious 
+  end 
+end 
